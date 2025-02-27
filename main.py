e2c82bb43280881854be73fc8e0d4d8ef46ee72a
tooba653 <164219929+tooba653@users.noreply.github.com>
2025-02-27 15:36:54 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("Hello World!!!!")
+
+def greet(name):
+    return f"Hello,{name}!"
+
+# print(greet("Tooba"))
+STRING
+var str ="HELLO WORLD!!!"
+# print(text)
+
+id_card:str= """ Name:Tooba
+Father Name : Muhammad Yameen
+Age : 20
+"""
+# print(id_card)
+
+NUMBER
+num:int = 89
+# print(type(num))
+
+FLOAT
+float_num : float =4.5
+# print (type(float_num))
+
+BOOLEAN
+
+is_true :bool= True
+is_false : bool = false
+# print(type(is_true))
+
+LIST
+fruit : list[str] = ["apple","banana","cherry"]
+# print(type(fruit))
+# print(fruit[0])
+# print(fruit[1])
+
+ TUPLE
+tuple_data :tuple = ("apple","cherry")
+# print(type(tuple_data))
+# print(tuple_data[0])
+# print(tuple_data[-1])
+
+DICTIONARY
+
+user_data :dict[str,str] ={
+    "name" :"Tooba",
+    "Father_name" :"Muhammad Yameen",
+}
+# print(type(user_data))
+# print(user_data)
+
